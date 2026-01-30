329d70fe4bad0e032c7cc95adf6dd78ac25c7c67
Hanqi <157480929+Elusywhq@users.noreply.github.com>
2026-01-30 17:47:54 +0800

    2026-01-30 17:47:54

--- pullgit.sh
+++ pullgit.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-git -C ~/bin/ORCA pull
-git -C ~/bin/Elusy_Personal pull
+if [[ $(hostname) == *"vanda"* ]]; then
+    git -C /atlas/home/$USER/bin/ORCA pull
+    git -C /atlas/home/$USER/bin/Elusy_Personal pull
+else
+    git -C ~/bin/ORCA pull
+    git -C ~/bin/Elusy_Personal pull
+fi
 exit 0
